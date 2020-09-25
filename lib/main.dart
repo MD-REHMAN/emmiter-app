1538006705ff2d6c329b05e4ae2ad44017058585
MD-REHMAN <Rehman.2468@gmail.com>
2020-09-25 13:42:24 +0530

    added logs to test

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,25 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+
+//     );
+//   }
+// }
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   String storeInitValue = 'Miles Morales';
   @override
   Widget build(BuildContext context) {
+    debugPrint('MyApp build called');
     return Provider(
         initValue: storeInitValue,
         child: MaterialApp(
@@ -20,21 +34,27 @@ class MyApp extends StatelessWidget {
             ),
             home: Scaffold(
               appBar: AppBar(title: Text('Shopping Mart')),
-              body: Center(
-                child: Row(
-                  children: <Widget>[
-                    Comp1(),
-                    Text('                  '),
-                    Comp2(),
-                  ],
-                ),
-              ),
+              body: Center(child: DummyWrapper()),
               floatingActionButton: FloatingActionButton(
                   onPressed: () {
-                    // Provider.setStore("Who's That?");
+                    debugPrint('setState called on MyAPP');
+                    setState(() {});
                   },
                   child: Icon(Icons.add_outlined)),
             )));
+  }
+}
+
+class DummyWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    debugPrint('DummyWrapper build called');
+    return Column(
+      children: <Widget>[
+        Comp1(),
+        Comp2(),
+      ],
+    );
   }
 }
 
@@ -44,11 +64,26 @@ class Comp1 extends StatefulWidget {
 }
 
 class _Comp1State extends State<Comp1> {
+  String planet = 'Earth 1700';
   @override
   Widget build(BuildContext context) {
+    debugPrint('Comp1 build called');
     Store store = Provider.of(context);
     store.onUpdate(() => setState(() {}));
     return Row(children: [
+      Text(planet),
+      FlatButton(
+        color: Colors.cyan,
+        textColor: Colors.white,
+        padding: EdgeInsets.all(18.0),
+        splashColor: Colors.blueAccent,
+        onPressed: () {
+          setState(() {
+            planet = 'Earth 2070';
+          });
+        },
+        child: Text('Change Planet'),
+      ),
       Text(store.getName()),
       FlatButton(
         color: Colors.cyan,
@@ -70,11 +105,32 @@ class Comp2 extends StatefulWidget {
 }
 
 class _Comp2State extends State<Comp2> {
+  String planet = 'Earth 1700';
   @override
+  void didUpdateWidget(Comp2 oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    debugPrint('Inside didUpdateWidget of Comp2');
+  }
+
   Widget build(BuildContext context) {
-    Store store = Provider.of(context, () => setState(() {}));
+    debugPrint('Comp2 build called');
+    Store store = Provider.of(context);
+    store.onUpdate(() => setState(() {}));
 
     return Row(children: [
+      Text(planet),
+      FlatButton(
+        color: Colors.cyan,
+        textColor: Colors.white,
+        padding: EdgeInsets.all(18.0),
+        splashColor: Colors.blueAccent,
+        onPressed: () {
+          setState(() {
+            planet = 'Mars 1900';
+          });
+        },
+        child: Text('Change Planet'),
+      ),
       Text(store.getName()),
       FlatButton(
         color: Colors.cyan,
@@ -84,8 +140,21 @@ class _Comp2State extends State<Comp2> {
         onPressed: () {
           store.updateName('Spider Man');
         },
-        child: Text('Wear Mask'),
+        child: Comp2Child('Wear Mask'),
       )
     ]);
+  }
+}
+
+class Comp2Child extends StatelessWidget {
+  String name;
+  Comp2Child(name) {
+    this.name = name;
+  }
+  @override
+  Widget build(BuildContext context) {
+    debugPrint('Comp2child build called');
+    debugPrint('------------------------------------- Last child in the tree');
+    return Text(name);
   }
 }
